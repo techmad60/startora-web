3cd16cd27f321ee168546afeaae44f20c44e03a9
Techmad <techmad60@gmail.com>
2026-09-19 11:24:59 +0100

    swept

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/discount",
+        destination: "/discount/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
